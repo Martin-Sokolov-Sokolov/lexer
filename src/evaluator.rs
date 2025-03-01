00d85e6581109fa0e4a2f18cd1b4bca690bd2714
Martin Sokolov <martin.sokolov.sokolov@gmail.com>
2025-03-01 17:35:12 +0100

    binaryop

--- src/evaluator.rs
+++ src/evaluator.rs
@@ -1,6 +1,5 @@
-use std::any::Any;
-
-use crate::parser::{Expr, Literal, UnaryOp};
+use std::{any::Any, process};
+use crate::parser::{BinaryOp, Expr, Literal, UnaryOp};
 
 pub struct Evaluator;
 
@@ -12,14 +11,14 @@ trait Visitor {
     fn visit_literal(&self, lit: &Literal) -> Option<Box<dyn Any>>;
     fn visit_grouping(&mut self, gr: &Box<Expr>) -> Option<Box<dyn Any>>;
     fn visit_unary(&mut self, op: &UnaryOp, un: &Box<Expr>) -> Option<Box<dyn Any>>;
+    fn visit_binary(&mut self, op: &BinaryOp, left: &Box<Expr>, right: &Box<Expr>) -> Option<Box<dyn Any>>;
 }
 
 impl Visitor for Evaluator {
     fn visit_literal(&self, expr: &Literal) -> Option<Box<dyn Any>> {
         match expr {
             Literal::Nil => Some(Box::new(Literal::Nil)),
-            Literal::False(b) => Some(Box::from(*b)),
-            Literal::True(b) => Some(Box::from(*b)),
+            Literal::Boolean(b) => Some(Box::from(*b)),
             Literal::Number(n) => Some(Box::from(*n)),
             Literal::Str(str) => Some(Box::new(str.clone())),
         }
@@ -41,6 +40,88 @@ impl Visitor for Evaluator {
             }
         }
     }
+
+    fn visit_binary(&mut self, op: &BinaryOp, left: &Box<Expr>, right: &Box<Expr>) -> Option<Box<dyn Any>> {
+        let _op_left = self.evaluate(&left)?;
+        let _op_right = self.evaluate(&right)?;
+
+        match op {
+            BinaryOp::Add => {
+                let (vl, vr) = (_op_left.downcast_ref::<f64>(), _op_right.downcast_ref::<f64>());
+                if let (Some(l), Some(r)) = (vl, vr) {
+                    return Some(Box::new(l + r))
+                }
+                let (vl, vr) = (_op_left.downcast_ref::<String>(), _op_right.downcast_ref::<String>());
+                if let (Some(l), Some(r)) = (vl, vr) {
+                    let mut res = String::from(l);
+                    res.push_str(r);
+                    return Some(Box::new(res));
+                }
+                None
+            }
+            BinaryOp::Subtract => {
+                let (vl, vr) = (_op_left.downcast_ref::<f64>(), _op_right.downcast_ref::<f64>());
+                if let (Some(l), Some(r)) = (vl, vr) {
+                    return Some(Box::new(l - r))
+                }
+                None
+            }
+            BinaryOp::Multiply => {
+                let (vl, vr) = (_op_left.downcast_ref::<f64>(), _op_right.downcast_ref::<f64>());
+                if let (Some(l), Some(r)) = (vl, vr) {
+                    return Some(Box::new(l * r))
+                }
+                None
+            }
+            BinaryOp::Divide => {
+                let (vl, vr) = (_op_left.downcast_ref::<f64>(), _op_right.downcast_ref::<f64>());
+                if let (Some(l), Some(r)) = (vl, vr) {
+                    if *r == 0.0 {
+                        process::exit(65);
+                    }
+                    return Some(Box::new(l / r))
+                }
+                None
+            }
+            BinaryOp::Greater => {
+                let (vl, vr) = (_op_left.downcast_ref::<bool>(), _op_right.downcast_ref::<bool>());
+                if let (Some(l), Some(r)) = (vl, vr) {
+                    return Some(Box::new(l > r))
+                }
+                None
+            }
+            BinaryOp::GreaterEqual => {
+                let (vl, vr) = (_op_left.downcast_ref::<bool>(), _op_right.downcast_ref::<bool>());
+                if let (Some(l), Some(r)) = (vl, vr) {
+                    return Some(Box::new(l >= r))
+                }
+                None
+            }
+            BinaryOp::Less => {
+                let (vl, vr) = (_op_left.downcast_ref::<bool>(), _op_right.downcast_ref::<bool>());
+                if let (Some(l), Some(r)) = (vl, vr) {
+                    return Some(Box::new(l < r))
+                }
+                None
+            }
+            BinaryOp::LessEqual => {
+                let (vl, vr) = (_op_left.downcast_ref::<bool>(), _op_right.downcast_ref::<bool>());
+                if let (Some(l), Some(r)) = (vl, vr) {
+                    return Some(Box::new(l <= r))
+                }
+                None
+            }
+            BinaryOp::EqualEqual => {
+                return Some(Box::new(self.is_equal(&_op_left, &_op_right)));
+            }
+            BinaryOp::NotEquals => {
+                return Some(Box::new(!self.is_equal(&_op_left, &_op_right)));
+            }
+            _ => return None,
+        }
+
+    }
+
 }
 
 impl Accept for Expr {
@@ -49,7 +130,7 @@ impl Accept for Expr {
             Expr::Lit(l) => visitor.visit_literal(l),
             Expr::Grouping(gr) => visitor.visit_grouping(gr),
             Expr::Unary(op, b) => visitor.visit_unary(op, b),
-            _ => None
+            Expr::Binary(left, op, right) => visitor.visit_binary(op, left, right),
         }
     }
 }
@@ -59,30 +140,54 @@ impl Evaluator {
         expr.accept(self)
     }
 
-    pub fn is_truthy(&self, r: &Box<dyn Any>) -> bool {
-        if r.is::<Literal>() {
+    fn is_equal(&self, a: &Box<dyn Any>, b: &Box<dyn Any>) -> bool {
+        if a.type_id() != b.type_id() {
             return false;
         }
+    
+        if let (Some(a_val), Some(b_val)) = (a.downcast_ref::<String>(), b.downcast_ref::<String>()) {
+            return a_val == b_val;
+        }
+        else if let (Some(a_val), Some(b_val)) = (a.downcast_ref::<f64>(), b.downcast_ref::<f64>()) {
+            return a_val == b_val;
+        }
+        else if let (Some(a_val), Some(b_val)) = (a.downcast_ref::<bool>(), b.downcast_ref::<bool>()) {
+            return a_val == b_val;
+        }
+    
+        false
+    }
 
-        if let Some(bool_val) = r.downcast_ref::<bool>() {
-            return *bool_val;
+    pub fn is_truthy(&self, r: &Box<dyn Any>) -> bool {
+        if r.is::<Literal>() {
+            if let Some(nil_val) = r.downcast_ref::<Literal>() {
+                match nil_val {
+                    Literal::Nil => return false,
+                    Literal::Boolean(b) => return *b,
+                    _ => return true,
+                }
+            }
         }
 
         return true;
     }
 
+
     pub fn writer(&self, value: &Box<dyn Any>) {
-        if let Some(val) = value.downcast_ref::<f64>() {
-            println!("{}", val);
+        if let Some(val) = value.downcast_ref::<Literal>() {
+            match val {
+                Literal::Nil => println!("nil"),
+                _ => (),
+            }
         }
-        else if let Some(_) = value.downcast_ref::<Literal>() {
-            println!("nil");
+        else if let Some(n) = value.downcast_ref::<f64>() {
+            println!("{}", n);
         }
-        else if let Some(val) = value.downcast_ref::<String>() {
-            println!("{}", val);
+        else if let Some(n) = value.downcast_ref::<String>() {
+            println!("{}", n);
         }
-        else if let Some(val) = value.downcast_ref::<bool>() {
-            println!("{}", val);
+        else if let Some(n) = value.downcast_ref::<bool>() {
+            println!("{}", n);
         }
         else {
             println!("not implemented");
